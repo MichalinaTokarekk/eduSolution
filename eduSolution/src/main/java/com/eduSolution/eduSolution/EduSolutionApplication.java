fc56a2531170ed7d27344ad3af78cdfff82a7f55
MichalinaTokarek <michalina.tokarek@gmail.com>
2023-09-20 12:50:31 +0200

    Initialize project

---
+++ eduSolution/src/main/java/com/eduSolution/eduSolution/EduSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.eduSolution.eduSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduSolutionApplication.class, args);
+	}
+
+}
