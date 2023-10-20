ced33dadee23cba3dc5172e612bbd5e9c39bc79a
MichalinaTokarek <michalina.tokarek@gmail.com>
2023-10-20 16:10:22 +0200

    hasAnyAuthority to course

--- eduSolution/src/main/java/com/eduSolution/eduSolution/config/SecurityConfiguration.java
+++ eduSolution/src/main/java/com/eduSolution/eduSolution/config/SecurityConfiguration.java
@@ -61,8 +61,8 @@ public class SecurityConfiguration {
 //                            .requestMatchers("/course-controller/courses").hasAnyAuthority("USER","LIBRARIAN","ADMIN")
 //                            .requestMatchers("/course-controller/course/{id}").hasAnyAuthority("USER","LIBRARIAN","ADMIN")
 //                            .requestMatchers("/course-controller/courseName/{name}").hasAnyAuthority("USER","LIBRARIAN","ADMIN")
-//                            .requestMatchers("/course-controller/**").hasAnyAuthority("ADMIN", "TEACHER", "STUDENT")
-                            .requestMatchers("/course-controller/**").permitAll()
+                            .requestMatchers("/course-controller/**").hasAnyAuthority("ADMIN", "TEACHER", "STUDENT")
+//                            .requestMatchers("/course-controller/**").permitAll()
 
 //                            .requestMatchers("/class-group-controller/**").hasAnyAuthority("ADMIN", "STUDENT")
                             .requestMatchers("/class-group-controller/**").permitAll()
